5715e6a89b2afad8876007a0244796d6d82d3937
programua <thestarsandletmeplayamong@gmail.com>
2018-11-04 01:39:07 +0900

    add assertion test

---
+++ server/arrayControle.js
@@ -0,0 +1,23 @@
+'use strict';
+module.exports = {
+    arrayPush,
+    arraySum
+}
+
+function arrayPush(num){
+    var arrayA = [];
+    for(var i = 0; i < num; i++){
+        arrayA.push(i);
+    }
+    return arrayA.length;
+}
+
+// 引数numの回数分乱数を生成
+// num個の要素を持つ配列を生成する
+function arraySum(num, callback){
+    var random = [];
+    for(var i=0; i<num; i++){
+        random.push(Math.round( Math.random()*100 ));
+    }
+    callback(random);
+}
---
+++ tests/arrayControleTest.js
@@ -0,0 +1,50 @@
+var assert = require('assert');
+var chai = require('chai');
+var arrayCtrl = require('../server/arrayControle');
+
+before(function(done){
+    console.log('[arrayControleTest] test start');
+    done();
+});
+after(function(done){
+    console.log('[arrayControleTest] test start');
+    done();
+});
+beforeEach(function(done){
+    console.log('[it] test start');
+    done();
+});
+afterEach(function(done){
+    console.log('[it] test end');
+    done();
+});
+
+describe('arrayControle', function(){
+        describe('arrayPush', function(){
+            it('入力した数と同じ要素数になること(1桁)', function(done){
+                assert.equal(8, arrayCtrl.arrayPush(8));
+                done();
+            });
+            it('入力した数と同じ要素数になること(2桁)', function(done){
+                assert.equal(10, arrayCtrl.arrayPush(10));
+                done();
+            });
+            it('入力した数と同じ要素数になること(3桁)', function(done){
+                assert.equal(100, arrayCtrl.arrayPush(100));
+                done();
+            });
+        });
+        describe('arraySum', function(){
+            it('戻り値が配列であること', function(done){
+                arrayCtrl.arrayPush(5, function(array){
+                    chai.assert.isArray(array, 'arrayCheck');
+                });
+                done();
+            });
+
+            // it('入力した数値分の要素を持った配列が返されること', function(done){
+                
+            // });
+        });
+    }
+);
